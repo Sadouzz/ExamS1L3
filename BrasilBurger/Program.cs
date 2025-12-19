a66f54e2fe9f9f5f6d56aa47f13682ff355e27ec
Sadouzz <ousmansadjo0@gmail.com>
2025-12-19 12:41:12 +0000

    Connexion avec la base de données Neon

--- BrasilBurger/Program.cs
+++ BrasilBurger/Program.cs
@@ -1,8 +1,15 @@
+using Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
