d7c553c8a01ab64c08374e95a0ec9a6cf7d0c25a
Sadouzz <ousmansadjo0@gmail.com>
2025-12-19 21:00:37 +0000

    Modification des vues

--- BrasilBurger/obj/Debug/net9.0/BrasilBurger.AssemblyInfo.cs
+++ BrasilBurger/obj/Debug/net9.0/BrasilBurger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BrasilBurger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+593c8d1815e3d83f192beb15455c175973df517e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d843461ebdb1cd29d6b2b0930007d615e940a333")]
 [assembly: System.Reflection.AssemblyProductAttribute("BrasilBurger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BrasilBurger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
