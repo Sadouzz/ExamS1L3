9882b63dfbf3c2b907f64f6857c96470c2383c21
Sadouzz <ousmansadjo0@gmail.com>
2025-12-25 21:50:19 +0000

    Pagination sur liste de commandes

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\DTO\CommandeDTO;
 use App\Repository\CommandeRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -15,12 +16,28 @@ final class CommandeController extends AbstractController
     }
 
     #[Route('/commande', name: 'app_commande')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
-        $commandes = $this->commandeRepository->findAll();
+        $page = $request->query->getInt('page', 1);
+        $limit = $this->getParameter('LIMIT_PER_PAGE');
+        $offset = ($page - 1) * $limit;
+
+        $count = $this->commandeRepository->count([]);
+        $nbrePages = ceil($count / $limit);
+
+        $commandes = $this->commandeRepository->findBy(
+            [],
+            ['id' => 'asc'],
+            $limit,
+            $offset
+        );
+
         $commandesDto = CommandeDto::fromEntities($commandes);
         return $this->render('commande/index.html.twig', [
             'commandes' => $commandesDto,
+            'count' => $count,
+            'pageEnCours' => $page,
+            'nbrePages' => $nbrePages,
             'controller_name' => 'CommandeController',
         ]);
     }
