f26b846585cd13524157f58f8ae8f3158b8d51d1
Sadouzz <ousmansadjo0@gmail.com>
2025-12-28 13:34:44 +0000

    Pagination sur les livraisons

--- src/Controller/LivraisonController.php
+++ src/Controller/LivraisonController.php
@@ -45,13 +45,29 @@ final class LivraisonController extends AbstractController
             $filtered = false;
         }
 
+        $page = $request->query->getInt('page', 1);
+        $limit = $this->getParameter('LIMIT_PER_PAGE');
+        $offset = ($page - 1) * $limit;
+
+        $count = $this->livraisonAffectationRepository->count($filtre);
+        $nbrePages = ceil($count / $limit);
+
+
         $livraisons = $this->livraisonAffectationRepository->findBy(
             $filtre,
             ['id' => 'DESC'],
+            $limit,
+            $offset,
         );
+
         $livraisonsDTO = LivraisonDTO::fromEntities($livraisons, $this->commandeRepository);
         return $this->render('livraison/index.html.twig', [
             'livraisons' => $livraisonsDTO,
+            'count' => $count,
+            'pageEnCours' => $page,
+            'nbrePages' => $nbrePages,
+            'limit' => $limit,
+            'filtered' => $filtered,
             'formSearchLivraison' => $form->createView(),
         ]);
     }
